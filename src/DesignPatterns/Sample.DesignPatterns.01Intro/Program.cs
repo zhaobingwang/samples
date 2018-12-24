9d59ff63fee19770e6bd35e4e681249454f63c78
Route8 <zhaobingwang@foxmail.com>
2018-12-24 22:38:33 +0800

    initialize new project

---
+++ src/DesignPatterns/Sample.DesignPatterns.01Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.DesignPatterns._01Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
