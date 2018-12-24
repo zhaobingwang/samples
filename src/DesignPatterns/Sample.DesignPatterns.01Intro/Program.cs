1d7597c6178a8e19467df0c949f9952d32780929
Route8 <zhaobingwang@foxmail.com>
2018-12-24 23:11:18 +0800

    design patterns 01 intro - temp commit

--- src/DesignPatterns/Sample.DesignPatterns.01Intro/Program.cs
+++ src/DesignPatterns/Sample.DesignPatterns.01Intro/Program.cs
@@ -6,7 +6,59 @@ namespace Sample.DesignPatterns._01Intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MallardDuck mallardDuck = new MallardDuck();
+            mallardDuck.Display();
+            mallardDuck.Quack();
+            mallardDuck.Swim();
+
+            RubberDuck rubberDuck = new RubberDuck();
+            rubberDuck.Display();
+            rubberDuck.Quack();
+            rubberDuck.Fly();
+        }
+    }
+    public abstract class Duck
+    {
+        public void Quack()
+        {
+            // 所有的鸭子都会呱呱叫，由Duck类负责实现
+            Console.WriteLine("呱呱叫...");
+        }
+        public void Swim()
+        {
+            // 所有鸭子都会游泳，由Duck类负责实现
+            Console.WriteLine("游泳中...");
+        }
+        public void Fly()
+        {
+            // 部分鸭子会飞
+            Console.WriteLine("飞行中...");
+        }
+        public abstract void Display();    // 每个鸭子的子类负责实现自己的display
+    }
+    public class MallardDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("我是绿头鸭.");
+        }
+    }
+    public class RedheadDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("我是红头鸭子.");
+        }
+    }
+    public class RubberDuck : Duck
+    {
+        public new void Quack()
+        {
+            Console.WriteLine("吱吱叫...");
+        }
+        public override void Display()
+        {
+            Console.WriteLine("我是橡皮鸭子.");
         }
     }
 }
