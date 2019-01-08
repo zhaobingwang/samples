9d34728c3bd257619d6fd7e2d25a66e037669ccf
Route8 <zhaobingwang@foxmail.com>
2019-01-08 21:13:28 +0800

    query by dapper

--- src/Core/Sample.Data.Access.Dapper/UsersOp.cs
+++ src/Core/Sample.Data.Access.Dapper/UsersOp.cs
@@ -2,26 +2,36 @@
 using MySql.Data.MySqlClient;
 using Sample.Data.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Sample.Data.Access.Dapper
 {
     public class UsersOp
     {
         MySqlConnection connection;
+        string _conncetionString = null;
         public UsersOp()
         {
-            connection = new MySqlConnection("server=127.0.0.1;user id=root;password=123456;database=sample");
+            _conncetionString = "server=127.0.0.1;user id=root;password=123456;database=sample";
         }
         public UsersOp(string connString)
         {
-            connection = new MySqlConnection(connString);
+            _conncetionString = connString;
         }
         public bool AddUser(User user)
         {
-            string sql = "insert into Users (NickName,Email,IsDelete,RegTime,ModifyTime,Remark) values (@nickName,@email,@isDelete,@regTime,@modifyTime,@remark)";
-            using (connection)
+            string sql = $"insert into Users ({nameof(User.NickName)},{nameof(User.Email)},{nameof(User.IsDelete)},{nameof(User.RegTime)},{nameof(User.ModifyTime)},{nameof(User.Remark)}) values (@NickName,@Email,@IsDelete,@RegTime,@ModifyTime,@Remark)";
+            using (MySqlConnection connection = new MySqlConnection(_conncetionString))
             {
                 return connection.Execute(sql, user) > 0;
+            }
+        }
+        public IEnumerable<User> GetUsersByNickName(string nickName)
+        {
+            string query = $"select {nameof(User.NickName)},{nameof(User.Email)},{(nameof(User.Remark))} from users a where {nameof(User.NickName)} =@NickName";
+            using (MySqlConnection connection = new MySqlConnection(_conncetionString))
+            {
+                return connection.Query<User>(query, new { nickName });
             }
         }
     }
--- src/Fragment/Sample.Fragment.ConsoleApp/Program.cs
+++ src/Fragment/Sample.Fragment.ConsoleApp/Program.cs
@@ -9,17 +9,26 @@ namespace Sample.Fragment.ConsoleApp
         static void Main(string[] args)
         {
             UsersOp usersOp = new UsersOp();
-            var result = usersOp.AddUser(new User
+
+            #region  add a suer
+            //var result = usersOp.AddUser(new User
+            //{
+            //    NickName = "张三",
+            //    Email = "zhangsan@qq.com",
+            //    IsDelete = false,
+            //    RegTime = DateTime.Now,
+            //    ModifyTime = DateTime.Now,
+            //    Remark = "这是一个测试用户"
+            //});
+            //Console.WriteLine(result);
+            //Console.WriteLine("end."); 
+            #endregion
+
+            var users = usersOp.GetUsersByNickName("张三");
+            foreach (var user in users)
             {
-                NickName = "张三",
-                Email = "zhangsan@qq.com",
-                IsDelete = false,
-                RegTime = DateTime.Now,
-                ModifyTime = DateTime.Now,
-                Remark = "这是一个测试用户"
-            });
-            Console.WriteLine(result);
-            Console.WriteLine("end.");
+                Console.WriteLine($"{user.NickName}\t{user.Email}\t{user.Remark}");
+            }
         }
     }
 }
