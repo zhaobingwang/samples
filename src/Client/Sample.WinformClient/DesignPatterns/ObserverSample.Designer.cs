d47b624ea26f21efe96cf357e7590f987c15b156
Route8 <zhaobingwang@foxmail.com>
2019-01-10 22:05:47 +0800

    观察者模式智能家居示例-temp commit

---
+++ src/Client/Sample.WinformClient/DesignPatterns/ObserverSample.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Sample.WinformClient.DesignPatterns
+{
+    partial class ObserverSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessLogs = new System.Windows.Forms.RichTextBox();
+            this.SmartWatchClock = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ProcessLogs
+            // 
+            this.ProcessLogs.Location = new System.Drawing.Point(12, 63);
+            this.ProcessLogs.Name = "ProcessLogs";
+            this.ProcessLogs.Size = new System.Drawing.Size(776, 374);
+            this.ProcessLogs.TabIndex = 0;
+            this.ProcessLogs.Text = "";
+            // 
+            // SmartWatchClock
+            // 
+            this.SmartWatchClock.AutoSize = true;
+            this.SmartWatchClock.Font = new System.Drawing.Font("Microsoft YaHei", 12F);
+            this.SmartWatchClock.Location = new System.Drawing.Point(13, 28);
+            this.SmartWatchClock.Name = "SmartWatchClock";
+            this.SmartWatchClock.Size = new System.Drawing.Size(163, 21);
+            this.SmartWatchClock.TabIndex = 1;
+            this.SmartWatchClock.Text = "1970-01-01 00:00:00";
+            // 
+            // ObserverSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SmartWatchClock);
+            this.Controls.Add(this.ProcessLogs);
+            this.Name = "ObserverSample";
+            this.Text = "ObserverSample";
+            this.Load += new System.EventHandler(this.ObserverSample_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ProcessLogs;
+        private System.Windows.Forms.Label SmartWatchClock;
+    }
+}
---
+++ src/Client/Sample.WinformClient/DesignPatterns/ObserverSample.cs
@@ -0,0 +1,92 @@
+﻿using Sample.Utilities.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormTimer = System.Windows.Forms.Timer;
+
+namespace Sample.WinformClient.DesignPatterns
+{
+    public partial class ObserverSample : Form
+    {
+        Logger logger;
+
+        private WinFormTimer timerSmartWatchClock;
+
+        DateTime alarmTime;
+        public ObserverSample()
+        {
+            InitializeComponent();
+
+            logger = new Logger(ProcessLogs);
+
+            alarmTime = DateTime.Now.AddSeconds(10);
+
+            timerSmartWatchClock = new WinFormTimer();
+            timerSmartWatchClock.Tick += SmartWatchClock_Tick;
+            timerSmartWatchClock.Interval = 1000;
+            timerSmartWatchClock.Start();
+            
+
+        }
+
+        private void SmartWatchClock_Tick(object sender, EventArgs e)
+        {
+            SmartWatchClock.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        private void ObserverSample_Load(object sender, EventArgs e)
+        {
+            logger.Info($"闹钟时间设置为：{alarmTime}");
+            RunInBackgournd(alarmTime);
+        }
+
+
+        private void RunInBackgournd(DateTime? alarmTime)
+        {
+            if (alarmTime.HasValue)
+            {
+                var cancelToken = new CancellationTokenSource();
+                var task = new Task(() =>
+                {
+                    while (!cancelToken.IsCancellationRequested)
+                    {
+
+                        if (DateTime.Now >= alarmTime.Value)
+                        {
+                            //闹铃时间到了
+                            Alarm();
+                            cancelToken.Cancel();
+                        }
+                        else
+                        {
+                            //BeginInvoke(new MethodInvoker(() =>
+                            //{
+                                logger.Info($"现在时间是：{DateTime.Now}");
+                            //}));
+
+                        }
+                        Task.Delay(1000);
+                        cancelToken.Token.WaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+                    }
+                }, cancelToken.Token, TaskCreationOptions.LongRunning);
+                task.Start();
+            }
+        }
+
+        private void Alarm()
+        {
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                logger.Info("闹钟响...");
+            }));
+
+        }
+    }
+}
