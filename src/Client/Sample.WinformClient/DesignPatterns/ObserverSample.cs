3758d1531f94a8f2e24c507a5f251921a1440d11
Route8 <zhaobingwang@foxmail.com>
2019-01-11 20:32:32 +0800

    temp commit

--- src/Client/Sample.WinformClient/DesignPatterns/ObserverSample.cs
+++ src/Client/Sample.WinformClient/DesignPatterns/ObserverSample.cs
@@ -13,6 +13,12 @@ using WinFormTimer = System.Windows.Forms.Timer;
 
 namespace Sample.WinformClient.DesignPatterns
 {
+    /*
+     * 1 智能手表检测到您已睡醒（为了方便演示，本示例使用闹钟替代，闹钟响表示智能手表检测到您已睡醒）
+     * 2.1 打开床头灯
+     * 2.2 打开窗帘
+     * 2.3 播放音乐
+     */
     public partial class ObserverSample : Form
     {
         Logger logger;
@@ -32,7 +38,7 @@ namespace Sample.WinformClient.DesignPatterns
             timerSmartWatchClock.Tick += SmartWatchClock_Tick;
             timerSmartWatchClock.Interval = 1000;
             timerSmartWatchClock.Start();
-            
+
 
         }
 
@@ -44,49 +50,93 @@ namespace Sample.WinformClient.DesignPatterns
         private void ObserverSample_Load(object sender, EventArgs e)
         {
             logger.Info($"闹钟时间设置为：{alarmTime}");
-            RunInBackgournd(alarmTime);
+            TurnOnTheAlarm(alarmTime);
         }
 
 
-        private void RunInBackgournd(DateTime? alarmTime)
+        private void TurnOnTheAlarm(DateTime? alarmTime)
         {
-            if (alarmTime.HasValue)
+            if (!alarmTime.HasValue)
             {
-                var cancelToken = new CancellationTokenSource();
-                var task = new Task(() =>
-                {
-                    while (!cancelToken.IsCancellationRequested)
-                    {
-
-                        if (DateTime.Now >= alarmTime.Value)
-                        {
-                            //闹铃时间到了
-                            Alarm();
-                            cancelToken.Cancel();
-                        }
-                        else
-                        {
-                            //BeginInvoke(new MethodInvoker(() =>
-                            //{
-                                logger.Info($"现在时间是：{DateTime.Now}");
-                            //}));
-
-                        }
-                        Task.Delay(1000);
-                        cancelToken.Token.WaitHandle.WaitOne(TimeSpan.FromSeconds(1));
-                    }
-                }, cancelToken.Token, TaskCreationOptions.LongRunning);
-                task.Start();
+                return;
             }
+            var cancelToken = new CancellationTokenSource();
+            var task = new Task(() =>
+            {
+                while (!cancelToken.IsCancellationRequested)
+                {
+
+                    if (DateTime.Now >= alarmTime.Value)
+                    {
+                        // 闹铃时间到了
+                        Alarm();
+                        cancelToken.Cancel();
+                    }
+                    else
+                    {
+                        logger.Info($"现在时间是：{DateTime.Now}");
+                    }
+                    cancelToken.Token.WaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+                }
+            }, cancelToken.Token, TaskCreationOptions.LongRunning);
+            task.Start();
         }
 
         private void Alarm()
         {
-            BeginInvoke(new MethodInvoker(() =>
-            {
-                logger.Info("闹钟响...");
-            }));
+            logger.Info("闹钟响...");
+            AlarmClock alarmClock = new AlarmClock();
+            alarmClock.AlarmCustomEvent += TurnOnTheBedsideLamp;
+            alarmClock.AlarmCustomEvent += OpenThecurtains;
+            alarmClock.AlarmCustomEvent += PlayingMusic;
+        }
+        private void TurnOnTheBedsideLamp(object sender, CustomEventArgs e)
+        {
+            logger.Info("打开床头灯");
+        }
+        private void OpenThecurtains(object sender, CustomEventArgs e)
+        {
+            logger.Info("打开窗帘");
+        }
+        private void PlayingMusic(object sender, CustomEventArgs e)
+        {
+            logger.Info("播放音乐");
+        }
+    }
 
+    public class AlarmClock
+    {
+        public event EventHandler<CustomEventArgs> AlarmCustomEvent;
+
+        public void DoSomething(string s)
+        {
+            OnAlarmCustomEvent(new CustomEventArgs(s));
+        }
+        protected virtual void OnAlarmCustomEvent(CustomEventArgs e)
+        {
+            EventHandler<CustomEventArgs> handler = AlarmCustomEvent;
+            if (handler != null)
+            {
+                e.Message += $" at {DateTime.Now}";
+                handler(this, e);
+            }
+        }
+    }
+
+    public delegate void EventHandler(object sender, EventArgs e);
+
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string s)
+        {
+            message = s;
+        }
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
         }
     }
 }
