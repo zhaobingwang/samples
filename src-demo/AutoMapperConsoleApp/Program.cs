ca3a8f9c2ba56cbfb3848e0d8c99062c93e6f31e
Route8 <zhaobingwang@foxmail.com>
2019-02-24 16:15:13 +0800

    init

---
+++ src-demo/AutoMapperConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoMapperConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
