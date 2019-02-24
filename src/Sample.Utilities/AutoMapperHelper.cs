1c2e7bebbe981aa5e89717dc7f20a1178d4908c8
Route8 <zhaobingwang@foxmail.com>
2019-02-24 15:17:33 +0800

    AutoMapperHelper & unittest

---
+++ src/Sample.Utilities/AutoMapperHelper.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Sample.Utilities
+{
+    public static class AutoMapperHelper
+    {
+        public static TDest MapTo<TDest>(this object src)
+        {
+            return (TDest)Mapper.Map(src, src.GetType(), typeof(TDest));
+        }
+    }
+}
---
+++ test/Sample.Utilities.Tests/AutoMapperHelperTest.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+using Sample.Utilities;
+using AutoMapper;
+
+namespace Sample.Utilities.Tests
+{
+    [Trait("对象映射", "AutoMapper")]
+    public class AutoMapperHelperTest
+    {
+        public AutoMapperHelperTest()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<SourceClass, DestinationClass>();
+            });
+        }
+        [Fact(DisplayName = "正常入参")]
+        public void AutoMapperHelper_WithExpectedParameters()
+        {
+            // arrange
+            var src = new SourceClass { Id = 1 };
+
+            // act
+            var dest = AutoMapperHelper.MapTo<DestinationClass>(src);
+
+            // assert
+            Assert.NotNull(dest);
+            Assert.True(AreEqualsObjectValues(src, dest));
+        }
+
+        #region private function
+        private bool AreEqualsObjectValues(SourceClass src, DestinationClass dest)
+        {
+            return src.Id == dest.Id;
+        }
+        #endregion
+    }
+
+
+    class SourceClass
+    {
+        public int Id { get; set; }
+    }
+    class DestinationClass
+    {
+        public int Id { get; set; }
+    }
+}
