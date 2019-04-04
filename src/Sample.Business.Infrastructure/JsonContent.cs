4ffb74ac6c474164d9d668cdb13c9340d2c11093
zhaobingwang <zhaobingwang@foxmail.com>
2019-04-04 12:00:23 +0800

    修复一些错误

--- src/Sample.Business.Infrastructure/JsonContent.cs
+++ src/Sample.Business.Infrastructure/JsonContent.cs
@@ -7,7 +7,7 @@ namespace Sample.Business.Infrastructure
 {
     public class JsonContent : StringContent
     {
-        public JsonContent(object obj) : base(JsonOperator.ToJson<object>(obj), Encoding.UTF8, "application/json")
+        public JsonContent(object obj) : base(JsonOperator.ToJson(obj), Encoding.UTF8, "application/json")
         {
 
         }
