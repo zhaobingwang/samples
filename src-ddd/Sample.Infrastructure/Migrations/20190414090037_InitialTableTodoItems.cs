b081bcb6dfbde7d1b0ab4416934bf7cf8d6f90b1
zhaobingwang <zhaobingwang@foxmail.com>
2019-04-14 17:06:54 +0800

    使用新的数据库并添加数据库迁移

---
+++ src-ddd/Sample.Infrastructure/Migrations/20190414090037_InitialTableTodoItems.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sample.Infrastructure.Migrations
+{
+    public partial class InitialTableTodoItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
