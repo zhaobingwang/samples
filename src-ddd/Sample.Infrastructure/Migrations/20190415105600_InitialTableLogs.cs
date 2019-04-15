89e0e78abb239d5aa28c498f1f447fb28f0e1869
zhaobingwang <zhaobingwang@foxmail.com>
2019-04-15 18:57:06 +0800

    table migration：logs

---
+++ src-ddd/Sample.Infrastructure/Migrations/20190415105600_InitialTableLogs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sample.Infrastructure.Migrations
+{
+    public partial class InitialTableLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Application = table.Column<string>(nullable: true),
+                    Logged = table.Column<string>(nullable: true),
+                    Level = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true),
+                    Logger = table.Column<string>(nullable: true),
+                    Callsite = table.Column<string>(nullable: true),
+                    Exception = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    Action = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
