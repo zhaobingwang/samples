285b418ae5a8f6e457174c5c4c7991131bb8d16d
zhaobingwang <zhaobingwang@foxmail.com>
2019-04-15 17:04:48 +0800

    asp.net core web site

---
+++ src-ddd/Sample.Web/Controllers/TodoItemsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sample.Infrastructure.Repositories;
+
+namespace Sample.Web.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        private readonly ILogger<TodoItemsController> _logger;
+        TodoItemRepository _todoItemRepository;
+        public TodoItemsController(ILogger<TodoItemsController> logger, TodoItemRepository todoItemRepository)
+        {
+            _logger = logger;
+            _todoItemRepository = todoItemRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var todos = await _todoItemRepository.GetAllAsync();
+            return View(todos);
+        }
+    }
+}
