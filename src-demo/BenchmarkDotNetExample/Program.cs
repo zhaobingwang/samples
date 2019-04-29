cd993a7cbd359415717c9f20bd37e56b0a644656
zhaobingwang <zhaobingwang@foxmail.com>
2019-04-30 00:11:21 +0800

    BenchmarkDotNet 测试

--- src-demo/BenchmarkDotNetExample/Program.cs
+++ src-demo/BenchmarkDotNetExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Security.Cryptography;
 
 namespace BenchmarkDotNetExample
 {
@@ -6,7 +9,32 @@ namespace BenchmarkDotNetExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
         }
+    }
+
+    [CoreJob]
+    [RPlotExporter, RankColumn]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
     }
 }
