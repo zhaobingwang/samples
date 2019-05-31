6f1f81d38d43b2969cd25d40350e149b143b7659
zhaobingwang <zhaobingwang@foxmail.com>
2019-05-31 15:02:40 +0800

    [.NET 框架设计] 重温c#-灵活运用各元素：2.1.6 部分类，部分方法

---
+++ src/FrameworkDesign.ClassLibrary/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkDesign.ClassLibrary
+{
+    public partial class Employee
+    {
+        partial void GetJob()
+        {
+            Console.WriteLine("Partial method.");
+        }
+    }
+}
---
+++ src/FrameworkDesign.ClassLibrary/EmployeeTemp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkDesign.ClassLibrary
+{
+    public partial class Employee
+    {
+        public string Name { get; set; }
+        public void GetEmployeeFullName()
+        {
+
+        }
+        partial void GetJob();
+    }
+}
