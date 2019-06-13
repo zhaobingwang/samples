a091d72aefb298a058c8c01fdf099a3034ccb5d5
zhaobingwang <zhaobingwang@foxmail.com>
2019-06-13 15:13:54 +0800

    swagger for Sample.API.Framework

---
+++ src-ddd/Sample.API.Framework/Controllers/SwaggerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.API.Framework.Controllers
+{
+    public class SwaggerController : Controller
+    {
+        // GET: Swagger
+        public ActionResult Index()
+        {
+            return Redirect("/swagger");
+            //return View();
+        }
+    }
+}
