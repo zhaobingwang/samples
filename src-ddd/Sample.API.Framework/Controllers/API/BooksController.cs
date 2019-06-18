4674d9614d503500834e0b6640c6da3972c63297
zhaobingwang <zhaobingwang@foxmail.com>
2019-06-18 22:10:32 +0800

    update something

--- src-ddd/Sample.API.Framework/Controllers/API/BooksController.cs
+++ src-ddd/Sample.API.Framework/Controllers/API/BooksController.cs
@@ -4,12 +4,14 @@ using Sample.API.Framework.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Sample.API.Framework.Controllers.API
 {
     /// <summary>
     /// 书籍API
     /// </summary>
+    [RoutePrefix("api/books")]
     public class BooksController : BaseController
     {
 
@@ -17,6 +19,7 @@ namespace Sample.API.Framework.Controllers.API
         /// 获取所有书籍基本信息
         /// </summary>
         /// <returns></returns>
+        [Route("")]
         public IEnumerable<BookDto> GetBooks()
         {
             string sql = "select a.Title as title,b.Name as author,a.Genre as genre from Books a left join Authors b on a.AuthorId=b.AuthorId";
@@ -28,6 +31,8 @@ namespace Sample.API.Framework.Controllers.API
         /// </summary>
         /// <param name="id">书籍ID</param>
         /// <returns></returns>
+        [Route("{id:int}")]
+        [ResponseType(typeof(BookDto))]
         public IHttpActionResult GetBook(int id)
         {
             string sqlBook = $"select * from Books where {nameof(Book.BookId)}=@id";
