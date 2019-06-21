7564cf0e4c32d79c3aa04c037a04986089dca78d
zhaobingwang <zhaobingwang@foxmail.com>
2019-06-21 10:34:04 +0800

    update something

---
+++ src-project-dotnet-framework/Sample.API.Framework/Controllers/API/TemporaryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Sample.API.Framework.Controllers.API
+{
+    [RoutePrefix("api/temporary")]
+    public class TemporaryController : ApiController
+    {
+        [HttpGet]
+        [Route("t1")]
+        public IHttpActionResult T1()
+        {
+            Thread.Sleep(5000);
+            ThreadPool.GetMinThreads(out var minWorkerThreadCount, out var minIoThreadCount);
+            ThreadPool.GetMaxThreads(out var maxWorkerThreadCount, out var maxIoThreadCount);
+            //return Ok(DateTime.Now);
+            return Ok($"{minWorkerThreadCount}-{minIoThreadCount}| {maxWorkerThreadCount}-{maxIoThreadCount}");
+        }
+
+        [HttpGet]
+        [Route("t2")]
+        public async Task<IHttpActionResult> T2()
+        {
+            await Task.Delay(5000);
+            return Ok(DateTime.Now);
+        }
+    }
+}
