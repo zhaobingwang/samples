cff6bd2f889c415704faa12a7eb1e20017712386
zhaobingwang <zhaobingwang@foxmail.com>
2019-07-19 17:12:04 +0800

    .net core托管服务 on docker

---
+++ src-project-dotnetcore/Sample.NetCore.HostBackgroundTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample.NetCore.HostBackgroundTasks
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<TestService>();
+                services.AddLogging();
+            })
+            .ConfigureLogging((hostContext, configLogging) =>
+            {
+                //configLogging.AddConsole();
+                configLogging.SetMinimumLevel(LogLevel.Trace);
+                configLogging.AddNLog();
+                //configLogging.AddDebug();
+            })
+            .RunConsoleAsync();
+        }
+    }
+}
---
+++ src-project-dotnetcore/Sample.NetCore.HostBackgroundTasks/TestService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample.NetCore.HostBackgroundTasks
+{
+    public class TestService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private readonly ILogger<TestService> _logger;
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(TestService)} starting...");
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(TestService)} stoping...");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            _logger.LogInformation(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"));
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
