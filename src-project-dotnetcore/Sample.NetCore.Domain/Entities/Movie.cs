649cd3c4d159972f7ea036d595227aa2edeec1d0
zhaobingwang <zhaobingwang@foxmail.com>
2019-08-01 10:27:18 +0800

    添加验证 #24

--- src-project-dotnetcore/Sample.NetCore.Domain/Entities/Movie.cs
+++ src-project-dotnetcore/Sample.NetCore.Domain/Entities/Movie.cs
@@ -6,25 +6,34 @@ using System.Text;
 
 namespace Sample.NetCore.Domain.Entities
 {
+    // 关于System.ComponentModel.DataAnnotations Namespace
+    // https://docs.microsoft.com/zh-cn/dotnet/api/system.componentmodel.dataannotations?view=netcore-2.2
     public class Movie : BaseEntity
     {
         [Display(Name = "电影名")]
         [Required(ErrorMessage = "电影名字是必填的内容")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "电影名字长度范围在3~60")]
         public string Title { get; set; }
 
-        // 关于System.ComponentModel.DataAnnotations Namespace
-        // https://docs.microsoft.com/zh-cn/dotnet/api/system.componentmodel.dataannotations?view=netcore-2.2
         [Display(Name = "发行日期")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "类型")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string Genre { get; set; }
 
         [Display(Name = "售价")]
+        [Range(1, 100, ErrorMessage = "售价范围在1~100")]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [Display(Name = "级别")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
