3487325c80139a72d7f2356d4574fbbb14176fb1
zhaobingwang <zhaobingwang@foxmail.com>
2019-08-07 10:11:26 +0800

    修改一些发布至docker测试环境的内容

---
+++ src-project-dotnetcore/Sample.NetCore.Producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.NetCore.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- src-project-dotnetcore/Sample.NetCore.Web/Startup.cs
+++ src-project-dotnetcore/Sample.NetCore.Web/Startup.cs
@@ -60,9 +60,10 @@ namespace Sample.NetCore.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseExceptionHandler("/Home/Error");
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
