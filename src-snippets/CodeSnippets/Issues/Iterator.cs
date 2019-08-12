fa357032d2dbb8be0d1d52a53053f96ae0b86ad8
zhaobingwang <zhaobingwang@foxmail.com>
2019-08-12 19:07:17 +0800

    C# 迭代器

---
+++ src-snippets/CodeSnippets/Issues/Iterator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippets.Issues
+{
+    /// <summary>
+    /// 迭代器
+    /// https://www.cnblogs.com/yangecnu/archive/2012/03/17/2402432.html
+    /// </summary>
+    public class Iterator
+    {
+        public class Timetable
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+
+            public IEnumerable<DateTime> DateRange
+            {
+                get
+                {
+                    for (DateTime day = StartDate; day <= EndDate; day = day.AddDays(1))
+                    {
+                        yield return day;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<string> ReadLines(string fileName)
+        {
+            using (TextReader reader = File.OpenText(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
--- src-snippets/Temporary/Program.cs
+++ src-snippets/Temporary/Program.cs
@@ -7,20 +7,39 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace Temporary
 {
-    class A
-    {
-        public int MyProperty { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                _023.Run2();
+                Iterator.Timetable timetable = new Iterator.Timetable();
+                timetable.StartDate = Convert.ToDateTime("2019-08-01");
+                timetable.EndDate = DateTime.Now;
+
+                // 从时间段中迭代日期
+                // 使用循环
+                for (DateTime day = timetable.StartDate; day < timetable.EndDate; day = day.AddDays(1))
+                {
+                    Console.WriteLine(day);
+                }
+                // 循环有时没有迭代直观和有表现力，在本例中，可以理解为“时间区间中的每一天”，这正是foreach使用的场景。因此上述循环如果写成迭代，代码会更美观：
+                foreach (var day in timetable.DateRange)
+                {
+                    Console.WriteLine(day);
+                }
+
+                var json = JsonConvert.SerializeObject(timetable);
+                Console.WriteLine(json);
+
+                foreach (var line in Iterator.ReadLines("Files/TextFile.txt"))
+                {
+                    Console.WriteLine(line);
+                }
             }
             catch (CustomException ex)
             {
