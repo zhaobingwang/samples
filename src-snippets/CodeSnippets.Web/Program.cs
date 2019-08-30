db683d777fdcc2e5be9d5dad212f581e93410610
zhaobingwang <zhaobingwang@foxmail.com>
2019-08-30 17:29:06 +0800

    clean up

--- src-snippets/CodeSnippets.Web/Program.cs
+++ src-snippets/CodeSnippets.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AspNetCoreSecurity
+namespace CodeSnippets.Web
 {
     public class Program
     {
