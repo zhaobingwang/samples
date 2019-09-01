0f1632ca40ffa0a9f8e15f19f92fa95aff7fc6b4
zhaobingwang <zhaobingwang@foxmail.com>
2019-09-01 14:06:10 +0800

    Cookie-based认证

---
+++ src-snippets/CodeSnippets.WebMvc/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeSnippets.WebMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            var claims = new List<Claim>() {
+                new Claim(ClaimTypes.Name,"admin"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+
+            var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimIdentity));
+            return Ok();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+};
--- src-snippets/CodeSnippets.WebMvc/Program.cs
+++ src-snippets/CodeSnippets.WebMvc/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CodeSnippets.Web
+namespace CodeSnippets.WebMvc
 {
     public class Program
     {
