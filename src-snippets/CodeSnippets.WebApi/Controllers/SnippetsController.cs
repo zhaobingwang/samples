0da83491bc17fe6fb6f51077121bbfb0c5d9a4b4
zhaobingwang <zhaobingwang@foxmail.com>
2019-09-19 20:32:42 +0800

    HttpClientFactory temp commit

---
+++ src-snippets/CodeSnippets.WebApi/Controllers/SnippetsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSnippets.WebApi.Controllers
+{
+    /// <summary>
+    /// 一些还未归类的代码片段集合
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnippetsController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SnippetsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ActionResult<string>> Get()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetStringAsync("https://docs.microsoft.com/zh-cn/");
+            return result;
+        }
+    }
+}
--- src-snippets/CodeSnippets.WebApi/Startup.cs
+++ src-snippets/CodeSnippets.WebApi/Startup.cs
@@ -70,6 +70,7 @@ namespace CodeSnippets.WebApi
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
