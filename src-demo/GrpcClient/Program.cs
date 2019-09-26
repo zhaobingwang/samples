e3f2ee65e795ae938ef42757fb0d3821f8f9273b
zhaobingwang <zhaobingwang@foxmail.com>
2019-09-26 21:49:06 +0800

    hello grpc

--- src-demo/GrpcClient/Program.cs
+++ src-demo/GrpcClient/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcClient.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "张三" });
+            Console.WriteLine(reply.Message);
         }
     }
 }
