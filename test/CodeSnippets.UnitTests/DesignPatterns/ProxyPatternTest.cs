e2490cda24a63fbdc818b5945833d58d2f297fdf
zhaobingwang <zhaobingwang@foxmail.com>
2019-10-15 20:16:19 +0800

    设计模式-代理模式 静态代理 #36

---
+++ test/CodeSnippets.UnitTests/DesignPatterns/ProxyPatternTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeSnippets.DesignPatterns.ProxyPattern;
+using Xunit;
+
+namespace CodeSnippets.UnitTests.DesignPatterns
+{
+    [Trait("设计模式", "代理模式测试")]
+    public class ProxyPatternTest
+    {
+        [Fact(DisplayName = "静态代理-成功")]
+        public void StaticProxyShouldSuccess()
+        {
+            const string HI = "hi";
+            ISubject subject = new SubjectProxy();
+            var result = subject.SayHi(HI);
+            Assert.True(result == HI);
+        }
+    }
+}
