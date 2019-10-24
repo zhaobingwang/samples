73697e55338ee8dc4fa9ac7359abdef5e772d31b
zhaobingwang <zhaobingwang@foxmail.com>
2019-10-24 10:48:53 +0800

    feat(*) 添加winform项目，并添加PleasWait UI组件

---
+++ src-snippets/CodeSnippets.Winform/Home.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CodeSnippets.Winform
+{
+    partial class Home
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLoading = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlLoading
+            // 
+            this.pnlLoading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLoading.Location = new System.Drawing.Point(62, 68);
+            this.pnlLoading.Name = "pnlLoading";
+            this.pnlLoading.Size = new System.Drawing.Size(552, 307);
+            this.pnlLoading.TabIndex = 0;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlLoading);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLoading;
+    }
+}
+
---
+++ src-snippets/CodeSnippets.Winform/Home.cs
@@ -0,0 +1,53 @@
+﻿using CodeSnippets.Winform.UIModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeSnippets.Winform
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            PleaseWait pleaseWait = new PleaseWait(pnlLoading, Properties.Resources.loading_01, "正在加载第一个任务...");
+            pleaseWait.Show();
+
+            var backgroundScheduler = TaskScheduler.Default;
+            var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Task.Factory.StartNew(delegate
+            {
+                Thread.Sleep(2000);
+                cts.Cancel();
+                pleaseWait.Close();
+            }, token, TaskCreationOptions.None, backgroundScheduler)
+            .ContinueWith(delegate
+            {
+                pleaseWait.Refresh("正在加载第二个任务");
+            }, token, TaskContinuationOptions.None, uiScheduler)
+            .ContinueWith(delegate
+            {
+                Thread.Sleep(2000);
+            }, token, TaskContinuationOptions.None, backgroundScheduler)
+            .ContinueWith(delegate
+            {
+                pleaseWait.Refresh("正在加载第三个任务");
+                pleaseWait.Close();
+            }, token, TaskContinuationOptions.None, uiScheduler);
+        }
+    }
+}
