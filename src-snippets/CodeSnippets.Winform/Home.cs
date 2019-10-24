376442500b96ee3c3d2a4f4c72f3712fc93427cc
zhaobingwang <zhaobingwang@foxmail.com>
2019-10-24 11:10:30 +0800

    refactor(CodeSnippets.Winform) Task异常处理 in winform

--- src-snippets/CodeSnippets.Winform/Home.cs
+++ src-snippets/CodeSnippets.Winform/Home.cs
@@ -19,21 +19,41 @@ namespace CodeSnippets.Winform
             InitializeComponent();
         }
 
+        // 定义AggregateException 的事件委托
+        static event EventHandler<AggregateExceptionArgs> AggregateExceptionCatched;
         private void Home_Load(object sender, EventArgs e)
         {
             PleaseWait pleaseWait = new PleaseWait(pnlLoading, Properties.Resources.loading_01, "正在加载第一个任务...");
             pleaseWait.Show();
+            AggregateExceptionCatched += new EventHandler<AggregateExceptionArgs>(Program_AggregateExceptionCatched);
 
             var backgroundScheduler = TaskScheduler.Default;
             var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             CancellationTokenSource cts = new CancellationTokenSource();
             CancellationToken token = cts.Token;
 
+
             Task.Factory.StartNew(delegate
             {
-                Thread.Sleep(2000);
-                cts.Cancel();
-                pleaseWait.Close();
+                try
+                {
+                    Thread.Sleep(2000);
+                    ThrowTestException();
+                    cts.Cancel();
+                    pleaseWait.Close();
+                }
+                catch (Exception ex)
+                {
+                    cts.Cancel();
+                    pleaseWait.Close();
+                    AggregateExceptionArgs args = new AggregateExceptionArgs()
+                    {
+                        AggregateException = new AggregateException(ex)
+                    };
+                    // 使用主线程委托代理，处理子线程 异常
+                    // 这种方式没有阻塞 主线程或其他线程
+                    AggregateExceptionCatched?.Invoke(null, args);
+                }
             }, token, TaskCreationOptions.None, backgroundScheduler)
             .ContinueWith(delegate
             {
@@ -48,6 +68,29 @@ namespace CodeSnippets.Winform
                 pleaseWait.Refresh("正在加载第三个任务");
                 pleaseWait.Close();
             }, token, TaskContinuationOptions.None, uiScheduler);
+
+            //var complexTask = Task.WhenAll(task);
+            //complexTask.ContinueWith()
+        }
+
+        private void ThrowTestException()
+        {
+            throw new Exception("这是一个用于测试的异常");
+        }
+        static void Program_AggregateExceptionCatched(object sender, AggregateExceptionArgs e)
+        {
+            string errorMessage = null;
+            foreach (var item in e.AggregateException.InnerExceptions)
+            {
+                errorMessage += $"{item.Message}\n";
+            }
+            MessageBox.Show(errorMessage);
+        }
+
+        // 定义异常参数处理
+        public class AggregateExceptionArgs : EventArgs
+        {
+            public AggregateException AggregateException { get; set; }
         }
     }
 }
