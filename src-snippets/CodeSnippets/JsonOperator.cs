ab087bd0d754d994fd26af7518de010c2e1991d2
zhaobingwang <zhaobingwang@foxmail.com>
2019-11-01 11:17:30 +0800

    System.Text.Json 基本操作

---
+++ src-snippets/CodeSnippets/JsonOperator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CodeSnippets
+{
+    public class JsonOperator<T> where T : class
+    {
+        public static string ToJson(T model)
+        {
+            return JsonSerializer.Serialize(model);
+        }
+        public static T ToObject(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+
+    public class JsonSampleModel
+    {
+        [JsonIgnore]
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset RegTime { get; set; }
+        public bool IsOnline { get; set; }
+        public JsonSampleStatus Status { get; set; }
+        public long? TagId { get; set; }
+    }
+    public enum JsonSampleStatus
+    {
+        Activated = 1,
+        NonActivated = 2
+    }
+}
---
+++ test/CodeSnippets.UnitTests/Unclassified/JsonTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeSnippets.UnitTests.Unclassified
+{
+    [Trait("未分类", "Json")]
+    public class JsonTest
+    {
+        [Fact(DisplayName = "序列化为Json-成功")]
+        public void SerializeShouldSuccess()
+        {
+            JsonSampleModel model = new JsonSampleModel { ID = 1, Name = "张三", RegTime = DateTimeOffset.Now, IsOnline = true, Status = JsonSampleStatus.Activated, TagId = null };
+            var json = JsonOperator<JsonSampleModel>.ToJson(model);
+            Assert.NotNull(json);
+        }
+
+        [Fact(DisplayName = "反序列化为Model-成功")]
+        public void DeserializeShouldSuccess()
+        {
+            string json = "{\"ID\":1,\"Name\":\"\u5F20\u4E09\",\"RegTime\":\"2019-11-01T10:54:05.8940775+08:00\",\"IsOnline\":true,\"Status\":1,\"TagId\":null}";
+            var model = JsonOperator<JsonSampleModel>.ToObject(json);
+            Assert.NotNull(model);
+            Assert.True(model.ID == 0);
+            Assert.Null(model.TagId);
+        }
+    }
+}
