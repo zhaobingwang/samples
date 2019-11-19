288bf68295432f35c0fd02233d28fe116f434697
zhaobingwang <zhaobingwang@foxmail.com>
2019-11-19 20:05:42 +0800

    feat(CodeSnippets) BCL-使用System.Text.Json 做基本的json序列化和反序列化

---
+++ src-snippets/CodeSnippets/BCL/Json.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CodeSnippets.BCL
+{
+    public static class Json
+    {
+        public static string Serialize()
+        {
+            var rng = new Random();
+            WeatherForecast weatherForecast = new WeatherForecast
+            {
+                Location = "Hangzhou",
+                Temperature = rng.Next(-10, 45),
+                Date = DateTimeOffset.UtcNow,
+            };
+            return JsonSerializer.Serialize(weatherForecast);
+        }
+
+        public static string GetSummary()
+        {
+            var json = "{\"Location\":\"Hangzhou\",\"Temperature\":4,\"Date\":\"2019-11-19T11:55:46.0310797+00:00\"}";
+            var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(json);
+            return $"{weatherForecast.Location} {weatherForecast.Date.ToLocalTime().ToString("yyyy年MM月dd日")}的温度是{weatherForecast.Temperature} ℃";
+        }
+        class WeatherForecast
+        {
+            public string Location { get; set; }
+            public int Temperature { get; set; }
+            public DateTimeOffset Date { get; set; }
+        }
+    }
+}
--- src-snippets/Temporary/Program.cs
+++ src-snippets/Temporary/Program.cs
@@ -1,94 +1,22 @@
-﻿using CodeSnippets;
-using CodeSnippets.Books.CSharp157;
-using CodeSnippets.Issues;
+﻿using CodeSnippets.BCL;
 using System;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using CodeSnippets.CSharp;
-using CodeSnippets.Infrastructure;
-using CodeSnippets.ThirdParty;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.EntityFrameworkCore;
-using CodeSnippets.Infrastructure.Interfaces;
-using CodeSnippets.Infrastructure.Repositories;
-using System.Diagnostics;
-using CodeSnippets.Infrastructure.Dto;
 
 namespace Temporary
 {
-    class User
-    {
-        public int Id { get; set; }
-    }
-
-
     class Program
     {
-        public static ILoggerFactory LoggerFactory;
-        public static IConfigurationRoot Configuration;
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-
-            var services = new ServiceCollection();
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder().SetBasePath(Path.Combine(AppContext.BaseDirectory)).AddJsonFile("appsettings.json", optional: true);
-            Configuration = builder.Build();
-
-            services.AddEntityFrameworkSqlite()
-                .AddDbContext<SqliteEFContext>(options => options.UseSqlite(Configuration.GetConnectionString("sqlite")));
-            services.AddScoped<ISampleEntityRepository, SampleEntityRepository>();
-
-            var serviceProvider = services.BuildServiceProvider();
-            var sampleEntityService = serviceProvider.GetRequiredService<ISampleEntityRepository>();
-
-            Stopwatch stopwatch = new Stopwatch();
             try
             {
-                stopwatch.Start();
-                Console.OutputEncoding = Encoding.Unicode;
-
-                var json = await File.ReadAllTextAsync("static/emoji/face-smiling.json");
-                var emojis = JsonOperator.ToObject<FaceSmilingDto>(json);
-                foreach (var emoji in emojis.FaceSmiling)
-                {
-                    Console.Write(Emoji.EmojiCodeToUTF16String(emoji.Code));
-                }
-                Console.WriteLine($"\nElapsed time:{stopwatch.ElapsedMilliseconds}ms");
-                stopwatch.Stop();
-            }
-            catch (CustomException ex)
-            {
-                Console.WriteLine(ex);
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex);
-            }
-            catch (ArgumentOutOfRangeException ex)
-            {
-                Console.WriteLine(ex);
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine(ex.Message);
+                var json = Json.Serialize();
+                Console.WriteLine(json);
+                var summary = Json.GetSummary();
+                Console.WriteLine(summary);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                if (stopwatch.IsRunning)
-                    stopwatch.Stop();
-                Console.WriteLine("end");
+                Console.WriteLine(ex.ToString());
             }
         }
     }
