010afbf0ff77327d307127ae4ab314d7f8b0b961
No.8 <zhaobingwang@foxmali.com>
2019-12-11 19:37:23 +0800

    feat(TS) : ASP.NET Core中的typescript

---
+++ src-snippets/CodeSnippets.WebMvc/wwwroot/scripts/app.js
@@ -0,0 +1,6 @@
+function sayHello() {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+    return "Hello from " + compiler + " and " + framework + "!";
+}
+//# sourceMappingURL=app.js.map
