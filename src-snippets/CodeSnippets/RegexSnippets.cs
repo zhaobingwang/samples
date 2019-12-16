432eea375a1af89ca82077e9516734658932d2b5
zhaobingwang <zhaobingwang@foxmali.com>
2019-12-16 18:00:54 +0800

    feat(CodeSnippets) : 正则表达式 temp commit

---
+++ src-snippets/CodeSnippets/RegexSnippets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeSnippets
+{
+    public class RegexSnippets
+    {
+        public static bool IsMatch(string source, string pattern)
+        {
+            return Regex.IsMatch(source, pattern);
+        }
+
+        public static string Match(string source, string pattern)
+        {
+            return Regex.Match(source, pattern).Value;
+        }
+    }
+}
---
+++ test/CodeSnippets.UnitTests/RegexSnippetsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace CodeSnippets.UnitTests
+{
+    [Trait("CodeSnippets", "正则表达式")]
+    public class RegexSnippetsTest
+    {
+        [Theory(DisplayName = "是否匹配")]
+        [InlineData("hello", @"\blo", false)]
+        [InlineData("hello", @"lo\b", true)]
+        [InlineData("hello", @"lo\B", false)]
+        [InlineData("hello", @"\Bhe", false)]
+        [InlineData("hello", @"\Bel", true)]
+        [InlineData("hello", @"el\B", true)]
+        public void IsMatch(string source, string pattern, bool isMatch)
+        {
+            var isMatchResult = RegexSnippets.IsMatch(source, pattern);
+            Assert.True(isMatchResult == isMatch);
+        }
+    }
+}
