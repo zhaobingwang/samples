a0b009eeae585513b212e8670f721f3b8d72cbc9
zhaobingwang <zhaobingwang@foxmail.com>
2019-12-18 23:19:40 +0800

    feat(IS4) : add apis - temp commit

---
+++ src-snippets/CodeSnippets.IdentityCenter/Config.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeSnippets.IdentityCenter
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> Apis() => new List<ApiResource>
+            {
+                new ApiResource("api1","My API")
+            };
+    }
+}
