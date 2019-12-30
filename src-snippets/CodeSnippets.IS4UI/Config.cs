2f3793e5b1c720beb445c7acd0551243c3cf5bd4
zhaobingwang <zhaobingwang@foxmail.com>
2019-12-30 23:23:31 +0800

    feat(IS4UI) : add is4ui and config default C]client credentials

---
+++ src-snippets/CodeSnippets.IS4UI/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeSnippets.IS4UI
+{
+    public static class Config
+    {
+        // Defining an API Resource
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>{
+                new ApiResource("CodeSnippets.WebApi","CodeSnippets API")
+            };
+
+        // Defining Client
+        public static IEnumerable<Client> Clients =>
+            new List<Client> {
+                new Client
+                {
+                    ClientId="client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets={
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes={ "CodeSnippets.WebApi" }
+                }
+            };
+    }
+}
---
+++ src-snippets/CodeSnippets.IS4UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSnippets.IS4UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
