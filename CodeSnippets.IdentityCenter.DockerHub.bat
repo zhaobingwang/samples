13e39f2de6ac9d018d3cc697cdff5624b6c7c3ae
zhaobingwang <zhaobingwang@foxmali.com>
2020-01-02 13:14:19 +0800

    chore(CodeSnippets.IdentityCenter): 添加发布到docker hub的脚本

---
+++ CodeSnippets.IdentityCenter.DockerHub.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set tag=0.1
+set workdir=C:\publish\docker\CodeSnippets.IdentityCenter\
+set repo=zhaobingwang/codesnippets.identitycenter
+
+@echo Default Tag:%tag%
+@echo Default Working Directory:%workdir%
+@echo Default Repository:%repo%
+
+@echo Set variables(If no value is entered,the default value will be used)
+
+set /p tag=Tag:
+set /p workdir=Working directory:
+set /p repo=Repository:
+
+
+@echo Locate to the working directory
+cd %workdir%
+
+
+@echo Docker login:
+docker login
+
+@echo Docker build:
+docker build -t %repo%:%tag% .
+
+@echo Docker push
+docker push %repo%:%tag%
+
+pause
