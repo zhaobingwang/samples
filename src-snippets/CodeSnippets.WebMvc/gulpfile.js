6617fa704f9fab9d00501b044ec011050317b9da
zhaobingwang <zhaobingwang@foxmail.com>
2020-01-14 11:30:47 +0800

    chore(CodeSnippets.WebMvc):暂不编译TypeScript

--- src-snippets/CodeSnippets.WebMvc/gulpfile.js
+++ src-snippets/CodeSnippets.WebMvc/gulpfile.js
@@ -4,17 +4,17 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
-var del = require('del');
+//var gulp = require('gulp');
+//var del = require('del');
 
-var paths = {
-    scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map']
-};
+//var paths = {
+//    scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map']
+//};
 
-gulp.task('clean', function () {
-    return del(['wwwroot/scripts/**/*']);
-});
+//gulp.task('clean', function () {
+//    return del(['wwwroot/scripts/**/*']);
+//});
 
-gulp.task('default', function () {
-    return gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'));
-});
+//gulp.task('default', function () {
+//    return gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/scripts'));
+//});
