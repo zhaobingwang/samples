5b8e2abb56419d78ff82dec8e79f4ca92a9161ac
zhaobingwang <zhaobingwang@foxmail.com>
2020-01-15 22:48:16 +0800

    initialize CodeSnippets.IS4AspId

---
+++ src-snippets/CodeSnippets.IS4AspId/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CodeSnippets.IS4AspId.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
