830e466f20284903c1b68dd871602a9e8e75dcc6
zhaobingwang <zhaobingwang@foxmail.com>
2020-02-20 23:00:13 +0800

    test(CodeSnippets.WebApi): 集成FluentAssertions

--- src-snippets/CodeSnippets.WebApi/Controllers/SampleController.cs
+++ src-snippets/CodeSnippets.WebApi/Controllers/SampleController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeSnippets.Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CodeSnippets.WebApi.Controllers
 {
@@ -11,5 +14,19 @@ namespace CodeSnippets.WebApi.Controllers
     [ApiController]
     public class SampleController : ControllerBase
     {
+        private readonly SqliteDbContext _sqliteDbContext;
+        private readonly ILogger<SampleController> _logger;
+        public SampleController(SqliteDbContext sqliteDbContext, ILogger<SampleController> logger)
+        {
+            _sqliteDbContext = sqliteDbContext;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Get(int id)
+        {
+            var sampleData = await _sqliteDbContext.SampleEntity
+                .SingleOrDefaultAsync(s => s.Id == id);
+            return Ok(sampleData);
+        }
     }
 }
--- test/CodeSnippets.WebApi.UnitTests/SampleControllerTest.cs
+++ test/CodeSnippets.WebApi.UnitTests/SampleControllerTest.cs
@@ -1,4 +1,14 @@
+using CodeSnippets.Infrastructure.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using CodeSnippets.WebApi.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using CodeSnippets.Infrastructure.Entities;
 
 namespace CodeSnippets.WebApi.UnitTests
 {
@@ -6,8 +16,43 @@ namespace CodeSnippets.WebApi.UnitTests
     public class SampleControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task GetShouldSuccess()
         {
+            // Arrange
+            var dbContext = await GetSqliteDbContextAsync();
+            var loggerMoq = new Mock<ILogger<SampleController>>();
+            var logger = loggerMoq.Object;
+            var controller = new SampleController(dbContext, logger);
+
+            // Act
+            var response = await controller.Get(1);
+
+            // Assert
+            // Assert.IsInstanceOfType(response, typeof(OkObjectResult));
+
+            // Use FluentValidation Instead Assert
+            var result = response.Should().BeOfType<OkObjectResult>().Subject;
+            var sampleData = result.Value.Should().BeAssignableTo<SampleEntity>().Subject;
+            sampleData.Id.Should().Be(1);
+            sampleData.BoolValue.Should().BeFalse();
+            sampleData.StringValue.Should().Be("sample");
+        }
+
+        private async Task<SqliteDbContext> GetSqliteDbContextAsync()
+        {
+            var options = new DbContextOptionsBuilder<SqliteDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var sqliteDbContext = new SqliteDbContext(options);
+            sqliteDbContext.SampleEntity.Add(new Infrastructure.Entities.SampleEntity
+            {
+                Id = 1,
+                BoolValue = false,
+                DateTimeValue = DateTime.Now,
+                StringValue = "sample"
+            });
+            await sqliteDbContext.SaveChangesAsync();
+            return sqliteDbContext;
         }
     }
 }
