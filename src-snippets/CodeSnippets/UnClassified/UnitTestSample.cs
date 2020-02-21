ffa636ac2f8453c6f6a7571fb6be56b530795bea
zhaobingwang <zhaobingwang@foxmail.com>
2020-02-21 22:55:34 +0800

    feat: .NET Core之单元测试（一）：入门

---
+++ src-snippets/CodeSnippets/UnClassified/UnitTestSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSnippets.UnClassified
+{
+    /// <summary>
+    /// 单元测试示例
+    /// </summary>
+    public class UnitTestSample
+    {
+        public static bool IsStringLong(string val)
+        {
+            return val?.Length > 6;
+        }
+    }
+}
---
+++ test/CodeSnippets.UnitTests/Unclassified/UnitTestSampleTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CodeSnippets.UnClassified;
+
+namespace CodeSnippets.UnitTests.Unclassified
+{
+    public class UnitTestSampleTest
+    {
+        [Fact]
+        public void IsStringLong_ReturnTrue_WithExpectedParameters()
+        {
+            // Arrange
+            string val = "1234567";
+
+            // Act
+            var actual = UnitTestSample.IsStringLong(val);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void IsStringLong_ReturnTrue_WithNullParameters()
+        {
+            // Arrange
+            string val = null;
+
+            // Act
+            var actual = UnitTestSample.IsStringLong(val);
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        // 'val.Length>6' threw an exception of type 'System.NullReferenceException'
+    }
+}
+
