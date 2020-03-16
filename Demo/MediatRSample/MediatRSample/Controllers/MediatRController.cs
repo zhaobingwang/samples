1be7fe184a81dff8247c37df79e8bf694fe623d0
zhaobingwang <zhaobingwang@foxmail.com>
2020-03-16 17:37:59 +0800

    feat(Demo): MediatR

---
+++ Demo/MediatRSample/MediatRSample/Controllers/MediatRController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediatRSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MediatRController : ControllerBase
+    {
+        IMediator _mediator;
+        public MediatRController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [Route("cal")]
+        [HttpGet]
+        public async Task<double> Calculator([FromQuery]Calculator calculator)
+        {
+            return await _mediator.Send(calculator);
+        }
+
+        [Route("ping")]
+        [HttpGet]
+        public async Task<string> Ping()
+        {
+            await _mediator.Publish(new Ping());
+            return "success";
+        }
+    }
+
+}
---
+++ Demo/MediatRSample/MediatRSample/Event.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatRSample
+{
+    public class Ping : INotification
+    {
+    }
+    public class Pong1 : INotificationHandler<Ping>
+    {
+        public Task Handle(Ping notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Pong 1");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Pong2 : INotificationHandler<Ping>
+    {
+        public Task Handle(Ping notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Pong 2");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Calculator : IRequest<double>
+    {
+        public string Operator { get; set; } = "+";
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+    }
+
+    public class CalculatorHandler : IRequestHandler<Calculator, double>
+    {
+        public Task<double> Handle(Calculator request, CancellationToken cancellationToken)
+        {
+            double result = 0;
+            switch (request.Operator)
+            {
+                case "+":
+                    result = request.Num1 + request.Num2;
+                    break;
+                default:
+                    break;
+            }
+            return Task.FromResult(result);
+        }
+    }
+}
