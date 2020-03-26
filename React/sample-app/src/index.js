9748c5cb4d2759a669352115202cf08d8c329114
zhaobingwang <zhaobingwang@foxmail.com>
2020-03-26 12:51:40 +0800

    feat(React): JSX

--- React/sample-app/src/index.js
+++ React/sample-app/src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RenderingElements from './rendering-elements';
+import JSX from './JSX';
 import './index.css';
 
-// JSX
-const name = '叶文洁';
-const element1 = <h1>Hello,{name}!</h1>
 
-const user = {
-    firstName: '淼',
-    lastName: '汪'
-}
-function formatCnName(user) {
-    return user.lastName + ' ' + user.firstName;
-}
-const element2 = (
-    <h1>
-        Hello,{formatCnName(user)}!
-    </h1>
-)
+
 
 // ========================================
 
-ReactDOM.render(
-    // element1,
-    element2,
-    document.getElementById('root'),
-);
+// ReactDOM.render(
+//     // element1,
+//     element2,
+//     document.getElementById('root'),
+// );
