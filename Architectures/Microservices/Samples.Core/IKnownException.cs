bee8c8b0ad2a05f073e65808465e7c92cff546a2
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-01 19:49:24 +0800

    feat(Microservices):Custom exception

---
+++ Architectures/Microservices/Samples.Core/IKnownException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Core
+{
+    public interface IKnownException
+    {
+        string Message { get; }
+        int ErrorCode { get; }
+        object[] ErrorData { get; }
+    }
+}
---
+++ Architectures/Microservices/Samples.Core/KnowException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Core
+{
+    public class KnowException : IKnownException
+    {
+        public string Message { get; private set; }
+
+        public int ErrorCode { get; private set; }
+
+        public object[] ErrorData { get; private set; }
+
+        private readonly static IKnownException Unknown = new KnowException { Message = "未知错误", ErrorCode = 9999 };
+
+        public static IKnownException FromKnownException(IKnownException exception)
+        {
+            return new KnowException { Message = exception.Message, ErrorCode = exception.ErrorCode, ErrorData = exception.ErrorData };
+        }
+    }
+}
