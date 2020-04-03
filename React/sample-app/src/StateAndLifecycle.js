079a0c694e4068489dd2b1481f771735420e1d8f
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-03 21:08:53 +0800

    React-Main Concepts: State and Lifecycle #38

--- React/sample-app/src/StateAndLifecycle.js
+++ React/sample-app/src/StateAndLifecycle.js
@@ -17,14 +17,34 @@ import ReactDOM from "react-dom";
 //   tick();
 // }, 1000);
 
-
 // 将函数组件转换成 class 组件
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>现在是 {this.props.date.toLocaleTimeString()}</h2>
+        <h2>现在是 {this.state.date.toLocaleTimeString()}</h2>
       </div>
-    )
+    );
   }
 }
+
+ReactDOM.render(<Clock></Clock>, document.getElementById("root"));
