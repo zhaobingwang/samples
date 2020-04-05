cffc1d00b102a8c4d637ff8355ea6f189758c65b
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-05 22:16:39 +0800

    initialize react-demo-app(use ant design)

---
+++ React/antd-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
