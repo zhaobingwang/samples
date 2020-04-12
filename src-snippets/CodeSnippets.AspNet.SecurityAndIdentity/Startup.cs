4605c69b66263dd4a51b4d63deb549f8363bb8e2
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-12 22:09:16 +0800

    ASP.NET Core 标识服务配置

--- src-snippets/CodeSnippets.AspNet.SecurityAndIdentity/Startup.cs
+++ src-snippets/CodeSnippets.AspNet.SecurityAndIdentity/Startup.cs
@@ -34,6 +34,7 @@ namespace CodeSnippets.AspNet.SecurityAndIdentity
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddIdentityConfig();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
