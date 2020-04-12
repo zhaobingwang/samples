793d2e2789ff6f09ff748f2d238e2f92391b546f
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-12 15:25:08 +0800

    initialize CodeSnippets.AspNet.SecurityAndIdentity

---
+++ src-snippets/CodeSnippets.AspNet.SecurityAndIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSnippets.AspNet.SecurityAndIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
