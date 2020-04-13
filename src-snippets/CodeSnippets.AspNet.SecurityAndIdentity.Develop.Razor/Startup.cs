1f1ede49dd18a7755697bcc0cffb1912a6cee636
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-13 17:12:38 +0800

    集成注册登录功能

--- src-snippets/CodeSnippets.AspNet.SecurityAndIdentity.Develop.Razor/Startup.cs
+++ src-snippets/CodeSnippets.AspNet.SecurityAndIdentity.Develop.Razor/Startup.cs
@@ -22,7 +22,7 @@ namespace CodeSnippets.AspNet.SecurityAndIdentity.Develop.Razor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
