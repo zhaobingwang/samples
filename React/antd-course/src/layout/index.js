48162910dd0e5371c4ddaa68d8e05877d4c7cd35
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-16 14:21:29 +0800

    feat(antd-react): 侧边导航

--- React/antd-course/src/layout/index.js
+++ React/antd-course/src/layout/index.js
@@ -1,15 +1,44 @@
 import { Component } from "react";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
+import { DashboardOutlined, PieChartOutlined } from "@ant-design/icons";
 
 // Header, Footer, Sider, Content组件在Layout组件模块下
 const { Header, Content, Footer, Sider } = Layout;
+
+// 引入子菜单组件
+const SubMenu = Menu.SubMenu;
 
 class BasicLayout extends Component {
   render() {
     return (
       <Layout>
         <Sider width={256} style={{ minHeight: "100vh", color: "white" }}>
-          Sider
+          <div
+            style={{
+              height: "32px",
+              background: "rgba:(255,255,255,.2)",
+              margin: "16px",
+            }}
+          ></div>
+          <Menu theme="dark" mode="inline" defaultOpenKeys={["1"]}>
+            <Menu.Item key="1">
+              <PieChartOutlined />
+              <span>HelloWorld</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <DashboardOutlined />
+                  <span>Dashboard</span>
+                </span>
+              }
+            >
+              <Menu.Item key="2">分析页</Menu.Item>
+              <Menu.Item key="3">监控页</Menu.Item>
+              <Menu.Item key="4">工作台</Menu.Item>
+            </SubMenu>
+          </Menu>
         </Sider>
         <Layout>
           <Header
