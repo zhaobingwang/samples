8ac028eb6da7af66bb5c00508ac5ff26f9d4b6a9
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-16 17:27:15 +0800

    feat(antd-react): 模拟服务端数据（模拟出错及简单数据模拟）

--- React/antd-course/src/model/puzzlecards.js
+++ React/antd-course/src/model/puzzlecards.js
@@ -1,4 +1,5 @@
 import reqeust from "../util/request";
+import { message } from "antd";
 
 const delay = (millisecond) => {
   return new Promise((resolve) => {
@@ -18,19 +19,22 @@ export default {
 
       // see more info : https://github.com/15Dkatz/official_joke_api
       const endPointURI = "/dev/random_joke";
+      try {
+        const puzzle = yield call(reqeust, endPointURI);
+        yield put({ type: "addNewCard", payload: puzzle });
 
-      const puzzle = yield call(reqeust, endPointURI);
-      yield put({ type: "addNewCard", payload: puzzle });
+        yield call(delay, 3000);
 
-      yield call(delay, 3000);
+        const puzzle2 = yield call(reqeust, endPointURI);
+        yield put({ type: "addNewCard", payload: puzzle2 });
 
-      const puzzle2 = yield call(reqeust, endPointURI);
-      yield put({ type: "addNewCard", payload: puzzle2 });
+        yield call(delay, 3000);
 
-      yield call(delay, 3000);
-
-      const puzzle3 = yield call(reqeust, endPointURI);
-      yield put({ type: "addNewCard", payload: puzzle3 });
+        const puzzle3 = yield call(reqeust, endPointURI);
+        yield put({ type: "addNewCard", payload: puzzle3 });
+      } catch (e) {
+        message.error("数据获取失败"); // 打印错误信息
+      }
     },
   },
   reducers: {
