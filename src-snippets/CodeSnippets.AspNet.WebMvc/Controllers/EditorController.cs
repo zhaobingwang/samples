837c7fd97e78c6992224c9a0f5385c495e622722
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-20 17:07:56 +0800

    添加编辑器：Summernote

---
+++ src-snippets/CodeSnippets.AspNet.WebMvc/Controllers/EditorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSnippets.AspNet.WebMvc.Controllers
+{
+    public class EditorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Summernote()
+        {
+            return View();
+        }
+    }
+}
