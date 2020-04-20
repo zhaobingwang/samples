3ee6b4420c446fbdb4cbf3602f12c780c1ced435
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-20 23:22:49 +0800

    feat(CodeSnippets.AspNet.WebMvc): 添加富文本编辑器Summernote以及使用HtmlAgilityPack对HTML进行数据清洗

--- src-snippets/CodeSnippets.AspNet.WebMvc/Controllers/EditorController.cs
+++ src-snippets/CodeSnippets.AspNet.WebMvc/Controllers/EditorController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeSnippets.AspNet.WebMvc.Controllers
@@ -15,7 +17,21 @@ namespace CodeSnippets.AspNet.WebMvc.Controllers
 
         public IActionResult Summernote()
         {
+            var html = "<script>alert('hi')</script>";
+            ViewData["html"] = html;
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Summernote(PostData data)
+        {
+            return Ok();
+        }
+
+    }
+
+    public class PostData
+    {
+        public string Content { get; set; }
     }
 }
