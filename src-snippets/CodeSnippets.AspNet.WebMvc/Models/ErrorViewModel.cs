5550473df79b39768066057784f99a5252e8f6e1
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-20 15:46:00 +0800

    initialize CodeSnippets.AspNet.WebMvc

---
+++ src-snippets/CodeSnippets.AspNet.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeSnippets.AspNet.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
