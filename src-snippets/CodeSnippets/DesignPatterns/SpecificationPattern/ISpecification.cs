67c545dc10716c79a08126b6d35e22a39b557e06
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-22 21:24:16 +0800

    规约模式(Specification Pattern)

---
+++ src-snippets/CodeSnippets/DesignPatterns/SpecificationPattern/ISpecification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSnippets.DesignPatterns.SpecificationPattern
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T candidate);
+        ISpecification<T> And(ISpecification<T> specification);
+        ISpecification<T> Or(ISpecification<T> specification);
+        ISpecification<T> Not(ISpecification<T> specification);
+    }
+}
