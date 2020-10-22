385372aa8f6600f1fe0905686012035458a96103
zhaobingwang <zhaobingwang@foxmail.com>
2020-10-22 09:17:15 +0800

    initialize EchartsMvcDemo

---
+++ Demo/EchartsDemo/EchartsMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EchartsMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
