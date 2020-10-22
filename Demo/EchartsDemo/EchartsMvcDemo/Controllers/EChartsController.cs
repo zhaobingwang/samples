2d1b4bf2e8b2c366ee9ce1b8cef6265bb67175e6
zhaobingwang <zhaobingwang@foxmail.com>
2020-10-22 16:45:02 +0800

    feat: 新增echarts demo

---
+++ Demo/EchartsDemo/EchartsMvcDemo/Controllers/EChartsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EchartsMvcDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EchartsMvcDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EChartsController : ControllerBase
+    {
+        [HttpGet("sampledata")]
+        public IActionResult SampleData()
+        {
+            var vo = GetSampleDataVO();
+            //var result = new
+            //{
+            //    dimensions = dimensions,
+            //    source = source
+            //};
+            return Ok(vo);
+        }
+
+        [HttpGet("stacked_line_chart_data")]
+        public IActionResult stackedLineChartData()
+        {
+            var vo = GetStackedLineChartDataVO();
+            //var result = new
+            //{
+            //    dimensions = dimensions,
+            //    source = source
+            //};
+            return Ok(vo);
+        }
+
+        public DataSetVO GetSampleDataVO()
+        {
+            DataSetVO vo = new DataSetVO();
+
+            string[] dimensions = new string[] { "产品", "2018", "2019", "2020" };
+
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            dict1.Add("产品", "手机");
+            dict1.Add("2018", "1000.01");
+            dict1.Add("2019", "2000.02");
+            dict1.Add("2020", "5000.50");
+
+            Dictionary<string, string> dict2 = new Dictionary<string, string>();
+            dict2.Add("产品", "服饰");
+            dict2.Add("2018", "3000.01");
+            dict2.Add("2019", "5000.02");
+            dict2.Add("2020", "8000.50");
+
+            Dictionary<string, string> dict3 = new Dictionary<string, string>();
+            dict3.Add("产品", "日用");
+            dict3.Add("2018", "11000.01");
+            dict3.Add("2019", "12000.02");
+            dict3.Add("2020", "15000.50");
+
+            Dictionary<string, string> dict4 = new Dictionary<string, string>();
+            dict4.Add("产品", "电脑");
+            dict4.Add("2018", "21000.01");
+            dict4.Add("2019", "22000.02");
+            dict4.Add("2020", "25000.50");
+
+            List<Dictionary<string, string>> source = new List<Dictionary<string, string>>();
+            source.Add(dict1);
+            source.Add(dict2);
+            source.Add(dict3);
+            source.Add(dict4);
+
+            vo.Dimensions = dimensions;
+            vo.Source = source;
+
+            return vo;
+        }
+
+        public DataSetVO GetStackedLineChartDataVO()
+        {
+            DataSetVO vo = new DataSetVO();
+
+            string[] dimensions = new string[] { "流量入口", "邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎" };
+            List<Dictionary<string, string>> source = new List<Dictionary<string, string>>();
+
+            for (int i = 1; i < dimensions.Length; i++)
+            {
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+                dict.Add("流量入口", getWeekValue(i));
+
+                dict.Add("邮件营销", new Random().Next(100, 999).ToString());
+                dict.Add("联盟广告", new Random().Next(100, 999).ToString());
+                dict.Add("视频广告", new Random().Next(100, 999).ToString());
+                dict.Add("直接访问", new Random().Next(100, 999).ToString());
+                dict.Add("搜索引擎", new Random().Next(100, 999).ToString());
+                source.Add(dict);
+            }
+
+
+            vo.Dimensions = dimensions;
+            vo.Source = source;
+
+            return vo;
+        }
+
+        private string getWeekValue(int i)
+        {
+            string val = "未知";
+            switch (i)
+            {
+                case 1:
+                    val = "周一";
+                    break;
+                case 2:
+                    val = "周二";
+                    break;
+                case 3:
+                    val = "周三";
+                    break;
+                case 4:
+                    val = "周四";
+                    break;
+                case 5:
+                    val = "周五";
+                    break;
+                case 6:
+                    val = "周六";
+                    break;
+                case 7:
+                    val = "周日";
+                    break;
+                default:
+                    break;
+            }
+            return val;
+        }
+    }
+}
---
+++ Demo/EchartsDemo/EchartsMvcDemo/Models/DataSetVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchartsMvcDemo.Models
+{
+    /// <summary>
+    /// 用 dimensions 指定了维度的顺序。
+    /// 直角坐标系中，默认把第一个维度映射到 X 轴上，第二个维度映射到 Y 轴上。
+    /// </summary>
+    public class DataSetVO
+    {
+        public string[] Dimensions { get; set; }
+        public List<Dictionary<string, string>> Source { get; set; }
+    }
+}
